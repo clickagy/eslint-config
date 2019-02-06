f0e9e41138576bc392496104db288d041724f280
Cody Carrell <nysos3@gmail.com>
2019-02-06 11:09:18 -0500

    Add base config.

---
+++ base.js
@@ -0,0 +1,28 @@
+'use strict'
+
+module.exports = {
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  plugins: [
+    'import'
+  ],
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/no-unresolved': 'off',
+    'import/no-unassigned-import': 'off',
+    semi: [
+      'warn',
+      'never'
+    ],
+    'no-console': 'off'
+  }
+}
